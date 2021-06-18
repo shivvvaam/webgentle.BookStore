7d47722b1f566b30918e948c055d5c77d20f055c
shivam <singh.shivam97@yahoo.com>
2021-06-18 14:53:58 +0530

    signup  and create user using Identity

--- Webgentle.BookStore/Webgentle.BookStore/Controllers/AccountController.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/AccountController.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Webgentle.BookStore.Models;
+using Webgentle.BookStore.Repository;
 
 namespace Webgentle.BookStore.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
         [Route("signup")]
         public IActionResult Signup()
         {
@@ -17,11 +25,21 @@ namespace Webgentle.BookStore.Controllers
 
         [Route("signup")]
         [HttpPost]
-        public IActionResult Signup(SignUpUserModel userModel)
+        public async Task<IActionResult> Signup(SignUpUserModel userModel)
         {
             if(ModelState.IsValid)
             {
                 //write your code
+                var result =await _accountRepository.CreateUserAsync(userModel);
+                if(!result.Succeeded)
+                {
+                    foreach (var errorMessage in result.Errors)
+                    {
+                        ModelState.AddModelError("", errorMessage.Description);
+
+                    }
+                    return View(userModel);
+                }
                 ModelState.Clear();
             }
             return View();
--- Webgentle.BookStore/Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Startup.cs
@@ -44,6 +44,7 @@ namespace Webgentle.BookStore
 #endif
             services.AddScoped<IBookRepository, BookRepository>();
             services.AddScoped<ILanguageRepository, LanguageRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
