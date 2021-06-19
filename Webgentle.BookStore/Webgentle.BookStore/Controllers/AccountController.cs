a2b0e39fa0a93d89f47500874d9db56b2806a967
shivam <singh.shivam97@yahoo.com>
2021-06-19 13:55:22 +0530

    Change password using core identity

--- Webgentle.BookStore/Webgentle.BookStore/Controllers/AccountController.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/AccountController.cs
@@ -80,5 +80,31 @@ namespace Webgentle.BookStore.Controllers
             return RedirectToAction("Index","Home");
         }
 
+        [Route("change-password")]
+        public  IActionResult ChangePassword()
+        {
+
+            return View();
+        }
+
+        [HttpPost("change-password")]
+        public async Task<IActionResult> ChangePassword(ChangePasswordModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =await _accountRepository.ChangePasswordAsync(model);
+                if (result.Succeeded)
+                {
+                    ViewBag.IsSuccess = true;
+                    ModelState.Clear();
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
     }
 }
